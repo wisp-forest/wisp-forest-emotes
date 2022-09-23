eac82c23b3043a369087698d29265e9f84d2164d
Dragon-Seeker <timekeeperguild@gmail.com>
2022-09-23 16:05:12 -0500

    Update to 1.19.2

--- src/main/java/ellemes/aofemotes/mixin/CommandSuggestorMixin.java
+++ src/main/java/ellemes/aofemotes/mixin/CommandSuggestorMixin.java
@@ -1,6 +1,6 @@
 package ellemes.aofemotes.mixin;
 
-import net.minecraft.client.gui.screen.CommandSuggestor;
+import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import net.minecraft.client.network.ClientCommandSource;
 import ellemes.aofemotes.emotes.EmoteRegistry;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,18 +11,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-@Mixin({CommandSuggestor.class})
+@Mixin({ChatInputSuggestor.class})
 public class CommandSuggestorMixin {
     @Redirect(
             method = {"refresh"},
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/network/ClientCommandSource;getPlayerNames()Ljava/util/Collection;"
+                    target = "Lnet/minecraft/client/network/ClientCommandSource;getChatSuggestions()Ljava/util/Collection;"
             )
     )
     private Collection<String> breakRenderedChatMessageLines(ClientCommandSource clientCommandSource) {
         List<String> suggestions = new ArrayList<>();
-        suggestions.addAll(clientCommandSource.getPlayerNames());
+        suggestions.addAll(clientCommandSource.getChatSuggestions());
         suggestions.addAll(EmoteRegistry.getInstance().getEmoteSuggestions());
         return suggestions;
     }
