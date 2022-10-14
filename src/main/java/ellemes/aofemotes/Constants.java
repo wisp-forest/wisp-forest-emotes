c5401f90d0701b220f9c78428f11d48535a6d5fa
glisco <glisco03@web.de>
2022-10-14 22:57:03 +0200

    🐼

--- src/main/java/ellemes/aofemotes/Constants.java
+++ src/main/java/ellemes/aofemotes/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.util.regex.Pattern;
 
 public class Constants {
-    public static final String MOD_ID = "aofemotes";
+    public static final String MOD_ID = "wisp-forest-emotes";
     public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
     public static final String EMOTE_PLACEHOLDER = "  ";
     public static final Pattern EMOTE_PATTEN = Pattern.compile("(?:\\s|^)(:([A-Za-z][A-Za-z0-9_-]*):)(?:\\s|$)");
